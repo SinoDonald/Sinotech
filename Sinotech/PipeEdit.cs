a76607e59ccb7641dc3611fff09944bccfecf9ef
SinoDonald <donaldlu@mail.sinotech.com.tw>
2025-10-01 16:25:03 +0800

    新增管線代碼與色彩修改功能, 撰寫中, 尚未同步至2020、2025版本

---
+++ Sinotech/PipeEdit.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows.Forms;
+using Form = System.Windows.Forms.Form;
+
+namespace Sinotech
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    [Journaling(JournalingMode.NoCommandData)]
+    public class PipeEdit : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // 檢查該 Form 是否已經開啟
+            Form myForm = Application.OpenForms["PipeEditForm"];
+
+            if (myForm == null)  // 尚未開啟
+            {
+                PipeEditForm pipeEditForm = new PipeEditForm(commandData.Application.ActiveUIDocument);
+                pipeEditForm.Show();
+            }
+            else // 已經開啟 -> 讓它跳到最前面
+            {
+                myForm.BringToFront();
+                myForm.WindowState = FormWindowState.Normal;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
